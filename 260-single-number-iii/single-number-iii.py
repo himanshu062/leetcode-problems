a66187573133e49e8736c5ff8dcc83473a7bbe07
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-05-31 11:21:28 +0530

    Time: 53 ms (82.38%) | Memory: 18.6 MB (60.05%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xors = functools.reduce(operator.xor, nums)
+        lowbit = xors & -xors
+        ans = [0, 0]
+        for num in nums:
+            if num & lowbit:
+                ans[0] ^= num
+            else:
+                ans[1] ^= num
+
+        return ans
