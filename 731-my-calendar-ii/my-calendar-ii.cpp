494a3f1cfabd08f4b13707daea8480432eac81b5
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-09-27 22:09:23 +0530

    Time: 127 ms (79.46%) | Memory: 42.8 MB (8.97%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.cpp
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -1,22 +1,25 @@
 class MyCalendarTwo {
 public:
     bool book(int start, int end) {
-        for (const auto& [s, e] : overlaps)
-            if (max(start, s) < min(end, e))
-                return false;
+        ++timeline[start];
+        --timeline[end];
 
-        for (const auto& [s, e] : ranges) {
-            const int ss = max(start, s);
-            const int ee = min(end, e);
-            if (ss < ee)
-                overlaps.emplace_back(ss, ee);
+        int activeEvents = 0;
+
+        for (const auto& [_, count] : timeline) {
+            activeEvents += count;
+            if (activeEvents > 2) {
+                if (--timeline[start] == 0)
+                    timeline.erase(start);
+                if (++timeline[end] == 0)
+                    timeline.erase(end);
+                return false;
+            }
         }
 
-        ranges.emplace_back(start, end);
         return true;
     }
 
 private:
-    vector<pair<int, int>> ranges;
-    vector<pair<int, int>> overlaps;
+    map<int, int> timeline;
 };
