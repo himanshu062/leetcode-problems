aae31c62db939e6f1576dbf33efce9316cec83d4
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-11-09 10:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (42.66%) - LeetSync

---
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        const int kMaxBit = log2(n) + log2(x) + 2;
+        const long k = n - 1;
+        long ans = x;
+        int kBinaryIndex = 0;
+
+        for (int i = 0; i < kMaxBit; ++i) {
+            if ((ans >> i & 1) == 0) {
+                if (k >> kBinaryIndex & 1)
+                    ans |= 1L << i;
+                ++kBinaryIndex;
+            }
+        }
+
+        return ans;
+    }
+};
