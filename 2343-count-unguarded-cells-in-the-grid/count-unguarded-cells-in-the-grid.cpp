3f488fb63a74162efbbbf66b077fed498e00dfdf
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-11-21 21:44:26 +0530

    Time: 8 ms (100.00%) | Memory: 150.9 MB (96.08%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -2,51 +2,32 @@ class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards,
                        vector<vector<int>>& walls) {
-        int ans = 0;
-        vector<vector<char>> grid(m, vector<char>(n));
-        vector<vector<char>> left(m, vector<char>(n));
-        vector<vector<char>> right(m, vector<char>(n));
-        vector<vector<char>> up(m, vector<char>(n));
-        vector<vector<char>> down(m, vector<char>(n));
-
-        for (const vector<int>& guard : guards)
-            grid[guard[0]][guard[1]] = 'G';
-
-        for (const vector<int>& wall : walls)
-            grid[wall[0]][wall[1]] = 'W';
-
-        for (int i = 0; i < m; ++i) {
-            char lastCell = 0;
-            for (int j = 0; j < n; ++j)
-                recordOrFill(grid[i][j], lastCell, left[i][j]);
-            lastCell = 0;
-            for (int j = n - 1; j >= 0; --j)
-                recordOrFill(grid[i][j], lastCell, right[i][j]);
+        int g[m][n];
+        memset(g, 0, sizeof(g));
+        for (auto& e : guards) {
+            g[e[0]][e[1]] = 2;
+        }
+        for (auto& e : walls) {
+            g[e[0]][e[1]] = 2;
+        }
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        for (auto& e : guards) {
+            for (int k = 0; k < 4; ++k) {
+                int x = e[0], y = e[1];
+                int dx = dirs[k], dy = dirs[k + 1];
+                while (x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n &&
+                       g[x + dx][y + dy] < 2) {
+                    x += dx;
+                    y += dy;
+                    g[x][y] = 1;
+                }
+            }
+        }
+        int unguardedCount = 0;
+        for (int i = 0; i < m; i++) {
+            unguardedCount += count(g[i], g[i] + n, 0);
         }
 
-        for (int j = 0; j < n; ++j) {
-            char lastCell = 0;
-            for (int i = 0; i < m; ++i)
-                recordOrFill(grid[i][j], lastCell, up[i][j]);
-            lastCell = 0;
-            for (int i = m - 1; i >= 0; --i)
-                recordOrFill(grid[i][j], lastCell, down[i][j]);
-        }
-
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                if (grid[i][j] == 0 && left[i][j] != 'G' &&
-                    right[i][j] != 'G' && up[i][j] != 'G' && down[i][j] != 'G')
-                    ++ans;
-
-        return ans;
-    }
-
-private:
-    void recordOrFill(char currCell, char& lastCell, char& infoCell) {
-        if (currCell == 'G' || currCell == 'W')
-            lastCell = currCell;
-        else
-            infoCell = lastCell;
+        return unguardedCount;
     }
 };
